22c29bd3ad17a259ae86f77d2dca9f4ddb9ec485
tobilg <tobias.mueller@mymood.de>
2015-04-30 11:44:59 +0200

    Fix for EXPOSE and VOLUME

--- 0.8.2.1/kafka-marathon-bootstrap.sh
+++ 0.8.2.1/kafka-marathon-bootstrap.sh
@@ -8,8 +8,8 @@ broker_id="${ip4}${PORT0}"
 
 # Write custom-server.properties config file
 echo "broker.id=${broker_id}" > $KAFKA_HOME/config/custom-server.properties
-echo "port=${PORT0}" >> $KAFKA_HOME/config/custom-server.properties
-echo "log.dir=/var/log/kafka-${broker_id}" >> $KAFKA_HOME/config/custom-server.properties
+echo "port=9092" >> $KAFKA_HOME/config/custom-server.properties
+echo "log.dir=/var/log/kafka/kafka-${broker_id}" >> $KAFKA_HOME/config/custom-server.properties
 echo "zookeeper.connect=${KAFKA_ZOOKEEPER_CONNECT}" >> $KAFKA_HOME/config/custom-server.properties
 
 exec $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/custom-server.properties
